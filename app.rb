132a648ee8e0d59c89153d5c32978ffdacf2d2f9
Jason Shockness <jshock12@gmail.com>
2019-08-06 19:52:46 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,15 @@
 class App < Sinatra::Base
+<<<<<<< HEAD
 # Define your GET '/' route below and respond with "Hello, World!"
   get '/' do 
     "Hello, World!"
   end
+=======
+
+  # Define your GET '/' route below and respond with "Hello, World!"
+  get '/' do 
+    "Hello, World!"
+  end
+
+>>>>>>> 8125629a2d7544eaadb43c76bf1f3cb8b2032626
 end
